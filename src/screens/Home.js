7bb0c4e6f5fdd1b78219ff60097fd252798fec81
Sadeep Dhananjana <sadeep@tecxaltd.com>
2021-09-12 01:15:05 +0530

    Configure basic project structure with navigation

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <Text>Hi, Nurse Bot</Text>
+    </View>
+  );
+};
+
+export default Home;
